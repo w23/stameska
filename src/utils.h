48fe7d1dbe2829d9d7a6b55c49d234eb3bb07241
Ivan Avdeev <me@w23.ru>
2018-11-21 22:54:25 +0700

    update visual studio project files

--- src/utils.h
+++ src/utils.h
@@ -21,4 +21,10 @@ public:
 #include "atto/app.h"
 #define MSG(...) aAppDebugPrintf(__VA_ARGS__)
 
-std::string format(const char* str, ...) __attribute__ ((format (printf, 1, 2)));
+#ifndef _WIN32
+#define PRINTF_ATTR(a, b) __attribute__ ((format (printf, a, b)))
+#else
+#define PRINTF_ATTR(a, b)
+#endif
+
+std::string format(const char* str, ...) PRINTF_ATTR(1, 2);
